22cca5c518b3ed71eb796c9714834e7f91a92a09
km2000-a11y <karim.m3_mts@gemsed.com>
2026-09-06 14:53:34 +0400

    sss

--- Scenes/track_select.gd
+++ Scenes/track_select.gd
@@ -22,6 +22,8 @@ func _on_split_pressed():
 
 
 func _on_back_btn_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/car_select.tscn")
-	if GameMode.game_mode=="Cop Chase":
+	if Modes.mode=="Cop Chase":
 		get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+
+	else: 
+		get_tree().change_scene_to_file("res://Scenes/car_select.tscn")
